b3b4a4caf6ed4150b19ccea9d573d453929463d9
florian <flo.moerzinger@gmx.at>
2020-07-11 16:56:19 +0200

    Review 2

---
+++ src/Exercise1.java
@@ -0,0 +1,8 @@
+public class Exercise1 {
+
+    public void start(){
+        System.out.println();
+        System.out.format( "%20s%n%n", " Start");
+        System.out.printf("%040d%n%n",0);
+    }
+}
