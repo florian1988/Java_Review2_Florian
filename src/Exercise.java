1ebef1aa59c9228a9fffcb1c886eb37aacb29cbb
florian <flo.moerzinger@gmx.at>
2020-07-11 13:51:44 +0200

    Initial commit

---
+++ src/Exercise.java
@@ -0,0 +1,17 @@
+public class Exercise {
+
+
+
+    String Exercise_name;
+    String Description;
+    int Duration;
+    String Position;
+
+    public void Exercise(String name, String Description, int Duration, String Position){
+        this.Exercise_name = name;
+        this.Description = Description;
+        this.Duration = Duration;
+        this.Position = Position;
+    }
+
+}
